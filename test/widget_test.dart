52670e25cfd51976fc0770c42876a5c3d32ea132
Vijay <vijay@agreeta.com>
2021-12-24 17:31:15 +0530

    Final Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sample/presentation/screens/main.dart';
+import 'package:sample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
